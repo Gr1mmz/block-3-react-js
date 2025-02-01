0b21cdb704c567de96f4dacbbcadc761f1d03422
Mikhail <mikhail.kraevskiy@gmail.com>
2025-02-01 19:21:53 +0800

    subscribe form

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,11 +1,12 @@
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
 
-import { MainAbout } from './blocks/main-about';
-import { MainBenefits } from './blocks/main-benefits';
-import { MainPackages } from './blocks/main-packages';
-import { MainPromo } from './blocks/main-promo';
-import { MainTeachers } from './blocks/main-teachers';
+import { MainAbout } from './components/main-about';
+import { MainBenefits } from './components/main-benefits';
+import { MainForm } from './components/main-form';
+import { MainPackages } from './components/main-packages';
+import { MainPromo } from './components/main-promo';
+import { MainTeachers } from './components/main-teachers';
 
 export const MainPage = () => {
   return (
@@ -17,6 +18,7 @@ export const MainPage = () => {
         <MainBenefits />
         <MainTeachers />
         <MainPackages />
+        <MainForm />
       </main>
       <Footer />
     </>
